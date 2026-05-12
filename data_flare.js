8013cc0738aa56434338357087d88f224fd83b79
BobotheHutt <jumprun4112@gmail.com>
2026-05-12 12:01:40 -0700

    Delete data_flare.js

--- data_flare.js
+++
@@ -1,8 +0,0 @@
-const data_flare = [
-    {n:"Baking Soda Emergency Dose", s:"Friendly", c:"Flare", d:"1/2 tsp in 8oz water for immediate 'lemon-on-cut' relief."},
-    {n:"Heating Pad / Ice Pack", s:"Friendly", c:"Flare", d:"Use on the pelvic floor to calm spasming muscles."},
-    {n:"Loose Clothing", s:"Friendly", c:"Flare", d:"Reduce 'Waistband pressure' to lower bladder pain."},
-    {n:"Deep Diaphragmatic Breathing", s:"Friendly", c:"Flare", d:"Calms the 'fight or flight' response that causes rapid filling."},
-    {n:"Azo (Phenazopyridine)", s:"Caution", c:"Flare", d:"Numbs the tract but can be hard on kidneys. Use only as needed."},
-    {n:"Urgent Doctor Call", s:"Avoid", d:"If you stop being able to pee at all, call the urologist immediately."}
-];
