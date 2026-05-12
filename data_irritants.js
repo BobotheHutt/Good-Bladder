fbe71c5f812bdde0261706ba7965b0d57d3901a3
BobotheHutt <jumprun4112@gmail.com>
2026-05-12 11:47:49 -0700

    Create data_irritants.js

---
+++ data_irritants.js
@@ -0,0 +1,8 @@
+const data_irritants = [
+    {n:"Citric Acid", s:"Avoid", c:"Irritants", d:"Found in almost all processed snacks. Major acid trigger."},
+    {n:"Artificial Sweeteners (Aspartame)", s:"Avoid", c:"Irritants", d:"Chemical bladder neurotoxin. Found in diet drinks."},
+    {n:"Red Dye 40", s:"Avoid", c:"Irritants", d:"Common coloring that causes chemical irritation in the bladder."},
+    {n:"Potassium Benzoate", s:"Avoid", c:"Irritants", d:"Preservative that frequently causes bladder spasms."},
+    {n:"Alcohol-based Flavors", s:"Avoid", c:"Irritants", d:"Common in vanilla or fruit extracts; irritates the bladder neck."},
+    {n:"MSG (Monosodium Glutamate)", s:"Avoid", c:"Irritants", d:"Increases nerve sensitivity and triggers frequency."}
+];
